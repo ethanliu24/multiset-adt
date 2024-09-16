e1b8d196871cbc5a063125e8ec5f0886773b6a09
izabellemarianne <izabellemarianne08@gmail.com>
2024-09-16 12:44:52 -0400

    added the constructor for the Tree class

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,19 @@
+import java.lang.reflect.Array;
+
 public class Tree {
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+
+    private int root;
+    private int[] _subtrees;
+    public Tree(int root, int[] subtrees) {
+        this.root = root;
+        if (subtrees == null){
+            this._subtrees = new int[0];
+        }
+        else{
+            this._subtrees = subtrees;
+        }
+
+    }
+
 }
